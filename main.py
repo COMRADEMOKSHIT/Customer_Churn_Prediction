164bf3a16a08af91c88178f1f969b8e73c942605
Mokshit <mokshitcomrade@gmail.com>
2026-08-03 22:45:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,9 +29,8 @@ def load_data(file) -> pd.DataFrame:
 
 DEFAULT_PATH = "WA_Fn-UseC_-Telco-Customer-Churn.csv"
 
-
-st.subheader("Data preview")
-st.dataframe(df.head())
+try:
+    df = load_data(DEFAULT_PATH)
 
 col1, col2 = st.columns(2)
 with col1:
