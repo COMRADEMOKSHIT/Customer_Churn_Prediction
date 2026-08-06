58e0e469dc4b97c48e1f0ba3b130610d23775e2a
Mokshit <mokshitcomrade@gmail.com>
2026-08-06 10:11:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import warnings
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 warnings.filterwarnings('ignore')
 
 import seaborn as sns
